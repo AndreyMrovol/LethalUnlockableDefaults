3e395cd2268504904dd56afa07496de7c264b344
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-01-24 10:02:04 +0100

    initial

---
+++ UnlockableDefaults/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace UnlockableDefaults
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        internal static ManualLogSource logger;
+        internal static Harmony harmony;
+
+        private void Awake()
+        {
+            logger = Logger;
+
+            harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
